d9305b4f45052edb684cd8b1a04b3b088a3c59e2
novakoda <novakoda9@gmail.com>
2020-04-28 14:47:26 -0500

    created user model & sign up/edit account views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
+class User::ParameterSanitizer < Devise::ParameterSanitizer
+  def initialize(*)
+    super
+    permit(:sign_up, keys: [:username, :name, :email])
+    permit(:account_update, keys: [:username, :name, :email])
+  end
+end
+
 class ApplicationController < ActionController::Base
+
+  protected
+
+  def devise_parameter_sanitizer
+    if resource_class == User
+      User::ParameterSanitizer.new(User, :user, params)
+    else
+      super # Use the default one
+    end
+  end
+
 end
