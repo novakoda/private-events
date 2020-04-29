c785f68943b46501066b0f9d368b80c669dd64a3
novakoda <novakoda9@gmail.com>
2020-04-28 23:50:17 -0500

    fixed user route

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
         sessions: 'users/sessions'
       }
 
-  get 'users/:id' => 'users#show', as: 'current_user'
+  get 'users/:id' => 'users#show', as: 'user'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "events#index"
