42111cdc2e9d79cefad6ecedf2cf59cfe630aab8
novakoda <novakoda9@gmail.com>
2020-04-28 22:57:02 -0500

    added user show page that shows events

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
         sessions: 'users/sessions'
       }
 
+  get 'users/:id' => 'users#show', as: 'current_user'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "events#index"
 end
