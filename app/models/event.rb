3eefbbedbda5a06b1346ac1a1dd20c2492a6e932
novakoda <novakoda9@gmail.com>
2020-04-28 20:13:00 -0500

    event host relationship works

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,4 @@
 class Event < ApplicationRecord
-  belongs_to :host, class_name: "Event"
-  has_and_belongs_to_many :attendees, class_name: "Attendee", foreign_key: "event_id", association_foreign_key: "attendee_id"
+  belongs_to :host, class_name: "User", foreign_key: "host_id"
+  has_and_belongs_to_many :attendees, class_name: "User", foreign_key: "event_id", association_foreign_key: "attendee_id"
 end
